6f9da51292fea7b3aff5270230b9d7efe374e55e
Charles Whittington <CharlesWhitt@gmail.com>
2025-10-30 02:54:16 -0400

    Indent contents of classes/methods/etc. (#81)
    Co-authored-by: Russell Keith-Magee <russell@keith-magee.com>

--- src/beeware_docs_tools/docs_test.py
+++ src/beeware_docs_tools/docs_test.py
@@ -1,8 +1,23 @@
 class DocsTest:
-    def __init__(self):
-        """This is the test class."""
+    def __init__(self, arg1: int, arg2: int = 42, **kwargs):
+        """This is the test class.
+
+        :param arg1: A value.
+        :param arg2: A value with a default.
+        :param kwargs: keyword arguments
+        """
         pass
 
-    def attribute(self):
+    @property
+    def attribute(self) -> int:
         """This is a test attribute."""
+        return 42
+
+    def method(self, arg1: int, arg2: int = 42, **kwargs):
+        """This is a test method.
+
+        :param arg1: A value.
+        :param arg2: A value with a default.
+        :param kwargs: keyword arguments
+        """
         pass
